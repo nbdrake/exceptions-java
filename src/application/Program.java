4f04f4b809b926add1b8fd066b3ad4ba97e0d3b9
Nathan Barreto <nbdrake007@gmail.com>
2023-08-11 23:00:29 -0400

    exceções ainda não tratadas

---
+++ src/application/Program.java
@@ -0,0 +1,39 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Account;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Informe os dados da conta");
+		System.out.print("Número: ");
+		int number = sc.nextInt();
+		sc.nextLine();
+		System.out.print("Titular: ");
+		String holder = sc.nextLine();
+		System.out.print("Saldo inicial: ");
+		double balance = sc.nextDouble();
+		System.out.print("Limite de saque: ");
+		double withdrawLimit = sc.nextDouble();
+		
+		Account acc = new Account(number, holder, balance, withdrawLimit);
+		
+		System.out.println();
+		System.out.print("Informe uma quantia para sacar: ");
+		double amount = sc.nextDouble();
+		
+		acc.withdraw(amount);
+		
+		System.out.printf("Novo saldo: %.2f%n", acc.getBalance());
+		
+		sc.close();
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author nbdra
+ *
+ */
+module aula_excecoes {
+}
